58a9d44d304e89236d4fb290e4a401670819ac9b
Tapan Raut <t.kumar.raut@accenture.com>
2022-04-30 11:41:36 +0530

    Initial Commit

---
+++ CryptoCurrency/CryptoCurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCurrencyApp.swift
+//  CryptoCurrency
+//
+//  Created by t.kumar.raut on 30/04/22.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
